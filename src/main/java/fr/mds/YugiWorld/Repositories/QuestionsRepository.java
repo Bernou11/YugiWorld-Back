06a5ce2833d984cfde7ef7cc8e757498b6a62c75
abrosse <thony95540@hotmail.fr>
2024-09-17 15:52:09 +0200

    Refacto modif bdd

--- src/main/java/fr/mds/YugiWorld/Repositories/QuestionsRepository.java
+++ src/main/java/fr/mds/YugiWorld/Repositories/QuestionsRepository.java
@@ -1,15 +1,9 @@
 package fr.mds.YugiWorld.Repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import fr.mds.YugiWorld.Entities.QuestionsEntity;
-import fr.mds.YugiWorld.Entities.ReponsesEntity;
 
 public interface QuestionsRepository extends JpaRepository<QuestionsEntity, Integer> {
-    @Query(nativeQuery=true, value="SELECT q.id, q.question, r.id, r.reponses FROM QuestionsEntity q, ReponsesEntity r WHERE r.question_id = :question_id")
-    List<ReponsesEntity> findByReponsesId(@Param("question_id") int questionId);
+
 }
--- src/main/java/fr/mds/YugiWorld/Services/QuestionService.java
+++ src/main/java/fr/mds/YugiWorld/Services/QuestionService.java
@@ -6,12 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import fr.mds.YugiWorld.Entities.QuestionReponseLienEntity;
 import fr.mds.YugiWorld.Entities.QuestionsEntity;
-import fr.mds.YugiWorld.Entities.ReponsesEntity;
-import fr.mds.YugiWorld.Repositories.QuestionReponseLienRepository;
 import fr.mds.YugiWorld.Repositories.QuestionsRepository;
-import java.util.ArrayList;
 
 @Service
 public class QuestionService {
@@ -19,27 +15,13 @@ public class QuestionService {
     @Autowired
     private QuestionsRepository questionRepository;
 
-    @Autowired
-    private QuestionReponseLienRepository questionReponseLienRepository;
-
     public QuestionsEntity createQuestion(String questionText) {
         QuestionsEntity question = new QuestionsEntity();
         question.setQuestion(questionText);
         return questionRepository.save(question);
     }
 
-    public List<ReponsesEntity> getLinkedReponses(int questionId) {
-        List<QuestionReponseLienEntity> liens = questionReponseLienRepository.findByQuestionId(questionId);
-
-        List<ReponsesEntity> reponsesLiees = new ArrayList<>();
-        for (QuestionReponseLienEntity lien : liens) {
-            reponsesLiees.add(lien.getReponse1()); 
-            reponsesLiees.add(lien.getReponse2());        
-        }
-        return reponsesLiees;
-    }
-
-    public List<QuestionsEntity> getAllQuestions() {
+    public List<QuestionsEntity> findAllQuestions() {
         return questionRepository.findAll();
     }
 
